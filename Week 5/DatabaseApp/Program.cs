9939cafeb50a7131bd63cf5504de5112a765018c
Mohammad Arfizur Rahman <arfizrahman0@gmail.com>
2025-01-06 15:15:49 +0600

    add week-5 project

---
+++ Week 5/DatabaseApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.SqlClient;
+
+string connectionString = "Server=localhost;Database=StudentDb;Trusted_Connection=True;";
+
+using(SqlConnection connection = new SqlConnection(connectionString)){
+    connection.Open();
+    Console.WriteLine("Connected to the database");
+
+    string query = "SELECT * FROM Students";
+    SqlCommand command = new SqlCommand(query, connection);
+    SqlDataReader reader = command.ExecuteReader();
+
+    while(reader.Read()){
+        Console.WriteLine($"ID: {reader["Id"]}, First Name: {reader["FirstName"]}, Last Name: {reader["LastName"]}, Date of Birth: {reader["DateOfBirth"]}");
+    }
+
+}
